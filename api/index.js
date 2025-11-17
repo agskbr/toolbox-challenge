bc9a512193233894325659f2a428ace0d40ce432
agskbr <acabrera@gett.mobi>
2025-11-17 13:14:03 -0300

    change cors config

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors({ origin: 'http://localhost:3000', methods: 'GET' }))
+app.use(cors({ origin: '*' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(router)
